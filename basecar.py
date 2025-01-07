9dc3c3664ac64a1bf623f4bf0fab24de9733855a
conner1811 <conner.drechsler@volkswagen.de>
2025-01-07 13:48:52 +0100

    Speed added

--- basecar.py
+++ basecar.py
@@ -22,3 +22,23 @@ class basecar:
 
     ''' steering angle of the car pi '''
     steering_angle = property(get_steering_angle, set_steering_angle)
+
+    ''' getter-method of speed'''
+    def get_speed(self):
+        return self._speed
+    
+    ''' setter-method of speed'''
+    def set_speed(self, speed):
+        print(f'speed to {speed}')
+        if speed < 0:
+            speed = 0
+        if speed > 100:
+            speed = 100
+        bw = BackWheels()
+
+        bw.speed = speed
+        self._speed = speed
+        #return self._speed
+
+    ''' speed of the car pi '''
+    speed = property(get_speed, set_speed)
