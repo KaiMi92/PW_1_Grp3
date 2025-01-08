b7f23028b37b5dcd1d1bc4c8f5fc03b601e9b893
Kai <kai.michel@me.com>
2025-01-08 14:49:47 +0100

    fix

--- test.py
+++ test.py
@@ -2,7 +2,6 @@ from basecar import *
 
 if __name__ == "__main__":
     auto = Auto()
-    auto.direction = 1
     auto.speed = 50
     auto.steering_angle = 135
     print(f"Gang: {auto.direction}, Speed: {auto.speed}")
