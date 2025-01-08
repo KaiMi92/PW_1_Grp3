5340a0420e7f8701321710328e4c1c593da31b20
Kai <kai.michel@me.com>
2025-01-08 09:47:10 +0100

    fix

--- basecar.py
+++ basecar.py
@@ -1,12 +1,14 @@
 from software.basisklassen import *
 
-class basecar():
+class basecar:
     def __init__(self, angle):
-        self.steering_ang = angle
+        self._steering_angle = angle
 
+    ''' getter-method of steering angle'''
     def get_steering_angle(self):
-        return self.steering_ang
+        return self._steering_angle
     
+    ''' setter-method of steering angle'''
     def set_steering_angle(self, angle):
         print(f'set angle to {angle}')
 
@@ -14,9 +16,29 @@ class basecar():
         set_angle = fw.turn(angle)
 
         print(f'angle was set to {set_angle}')
-        self.steering_ang = set_angle
+        self._steering_angle = set_angle
         
         time.sleep(1)
 
     ''' steering angle of the car pi '''
     steering_angle = property(get_steering_angle, set_steering_angle)
+
+    ''' getter-method of speed'''
+    def get_speed(self):
+        return self._speed
+    
+    ''' setter-method of speed'''
+    def set_speed(self, speed):
+        print(f'speed to {speed}')
+        if speed < 0:
+            speed = 0
+        if speed > 100:
+            speed = 100
+        bw = BackWheels()
+
+        bw.speed = speed
+        self._speed = speed
+        #return self._speed
+
+    ''' speed of the car pi '''
+    speed = property(get_speed, set_speed)
