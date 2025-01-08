cb4c0da9399353e70969a888267bce28d4e656c8
FrankN2005 <frank.nethe@volkswagen.de>
2025-01-08 19:45:57 +0100

    comments added

--- driving_mode_2.py
+++ driving_mode_2.py
@@ -3,30 +3,37 @@ from basecar import *
 # initial value from basecar 
 bc = BaseCar()
 my_speed = 60
-#my_angle
+my_angle = 135
 
 try:
+    #needed for video
+    time.sleep(3)
+    
     print(f'Start driving - set only speed')
     #bc.drive(speed = my_speed)
     bc.drive(speed = my_speed, steering_angle = 90)
     time.sleep(1)
-
+    #stop befor next step
     print(f'Stop driving')
     bc.stop()
     time.sleep(1)
 
+    #cycle driving
     print(f'Start driving - set only speed')
-    bc.drive(speed = my_speed, steering_angle = 135)
+    bc.drive(speed = my_speed, steering_angle = my_angle)
     time.sleep(8)
 
+    #stop befor next step
     print(f'Stop driving')
     bc.stop()
     time.sleep(1)
 
+    #cycle driving
     print(f'Start driving - set only speed')
-    bc.drive(speed = -my_speed, steering_angle = 135)
+    bc.drive(speed = -my_speed, steering_angle = my_angle)
     time.sleep(8)
 
+    #stop befor next step
     print(f'Stop driving')
     bc.stop()
     time.sleep(1)
