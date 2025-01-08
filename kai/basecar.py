523376faed0c10ae8aac4acdf5d160196e580123
Kai <kai.michel@me.com>
2025-01-08 13:46:53 +0100

    add drive

--- kai/basecar.py
+++ kai/basecar.py
@@ -32,15 +32,28 @@ class Auto:
     
     @speed.setter
     def speed(self, new_speed):
-        if new_speed < 0 and new_speed > -100:
+        if new_speed < 0 and new_speed >= -100:
             self._speed = new_speed
             print("Rückwärtsfahrt")
 
-        if new_speed > 0 and new_speed < 100:
+        if new_speed > 0 and new_speed <= 100:
             self._speed = new_speed
             print("Vorwärtsfahrt")
 
         else :
             self._speed = 0
             print("Stop")
-        return self._speed 
+        return self._speed
+
+
+    def drive(self, steering_angle, speed):
+        if steering_angle is None:
+            steering_angle = 90
+            return steering_angle
+        if speed is None:
+            speed = 0
+            return speed
+        else:
+            steering_angle = steering_angle
+            speed = speed
+            return steering_angle, speed
