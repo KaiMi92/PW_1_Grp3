4aa71aa65ac2491b2d6c01d78ed0e27057ed0ca0
FrankN2005 <frank.nethe@volkswagen.de>
2025-01-08 19:30:25 +0100

    new file mode2

---
+++ driving_mode_2.py
@@ -0,0 +1,28 @@
+from basecar import *
+
+# initial value from basecar 
+bc = BaseCar()
+my_speed = 30
+my_angle
+
+try:
+    print(f'Start driving - set only speed')
+    #bc.drive(speed = my_speed)
+    bc.drive(speed my_speed, steering_angle = 90)
+    time.sleep(1)
+
+    print(f'Stop driving')
+    bc.stop()
+    time.sleep(1)
+
+    print(f'Start driving - set only speed')
+    bc.drive(speed my_speed, steering_angle = 135)
+    time.sleep(8)
+
+except Exception as e:
+  print(f"An exception occurred: {e}") 
+  bc.stop()
+
+finally:
+  print("Everything ok!") 
+  bc.stop()
