e19dfa4a85442d2aa464b8c460c096fea801c2df
conner1811 <conner.drechsler@volkswagen.de>
2025-01-09 11:38:54 +0100

    added driving mode 3

--- driving_mode_3.py
+++ driving_mode_3.py
@@ -1,9 +1,22 @@
 from soniccar import *
 sc = SonicCar()
+my_speed = 40
+my_angle = 90
+STRAIGHT_FORWARD = my_angle
+MIN_DISTANCE = 20
 
-try:        
+
+
+try:  
+  sc.drive(speed = my_speed, steering_angle = STRAIGHT_FORWARD)      
+  
+  
+  while True:
     d = sc.get_distance()
-    print(f'd = {d}')
+    if d < MIN_DISTANCE and d > 0:
+      sc.stop()
+      break
+    time.sleep(0.5)
 
 except Exception as e:
   print(f"An exception occurred: {e}") 
