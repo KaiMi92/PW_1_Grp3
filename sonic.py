0cc3a4a64cdb565aee322431bd60c185e90a97d4
Kai <kai.michel@me.com>
2025-01-09 10:07:10 +0100

    add sonic car

---
+++ sonic.py
@@ -0,0 +1,18 @@
+from basecar import *
+from software.basisklassen import *
+import sys
+
+class SonicCar(Auto):
+    def __init__(self):
+        super().__init__()
+        self._us = Ultrasonic()
+
+    def get_sonic(self):
+        s = self._us.distance()
+        print(s)
+        return s
+
+
+    def save_data(self, s):
+        sonic_Dict = {}
+        sonic_Dict.update(s)
