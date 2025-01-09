07b8791cb0242b69db7168dadd890c3cab2056d7
KaiMi92 <kai.michel@me.com>
2025-01-09 14:06:44 +0100

    ad 5 trys

--- fp4.py
+++ fp4.py
@@ -6,11 +6,12 @@ if __name__ == "__main__":
     sonic = SonicCar()
     setspeed = 30
     turnangle = 90
+    trys = 0
 
     try:
         auto.drive(turnangle, setspeed)
 
-        while True:
+        while trys < 5:
             while True:
                 s = sonic.get_sonic()
                 if s < 20 and s > 0:
@@ -20,7 +21,8 @@ if __name__ == "__main__":
             
             auto.drive(135, -setspeed)
             time.sleep(2)
-            auto.drive(turnangle, setspeed)             
+            auto.drive(turnangle, setspeed)
+            trys +=1             
 
     except:
         print("error")
