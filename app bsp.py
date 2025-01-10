dc1a0be8eb783d9025af92e45e468d5c231fdda1
Kai <kai.michel@me.com>
2025-01-10 11:44:51 +0100

    test

--- app bsp.py
+++ app bsp.py
@@ -1,12 +1,12 @@
 from dash import Dash, html, dcc, Input, Output, State
 import pandas as pd
 import plotly.express as px
-from basecar import Auto
+#from basecar import Auto
 import dash_bootstrap_components as dbc
 
 app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
-auto = Auto()
+#auto = Auto()
 
 x = list(range(30))
 
--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from dash import Dash, html, dcc, Input, Output, State
 import pandas as pd
 import plotly.express as px
-from basecar import Auto
+#from basecar import Auto
 import dash_bootstrap_components as dbc
 
 app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
-auto = Auto()
+#auto = Auto()
+
+x = list(range(30))
+
+df = pd.DataFrame({"x": x, "y": [i**2 for i in x], "z": [i**0.5 for i in x]})
 
 app.layout = html.Div
