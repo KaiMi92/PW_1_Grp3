71dc5af24664c778d68c4673be40532a56a786ff
Sebastian <sebastian.lorenz@volkswagen.de>
2025-01-13 13:10:29 +0100

    remove writing log from soniccar

--- soniccar.py
+++ soniccar.py
@@ -2,17 +2,13 @@ from basecar import *
 from software.basisklassen import *
 import csv
 
-fieldnames = ['Time','Distance','Direction','Speed','Steering']
-setter_of_fieldnames = ['-','_distance','_direction','_speed','_steering_angle']
+fieldnames = ['Time','Distance','Direction','Speed','Steering','IR-v1','IR-v2','IR-v3','IR-v4','IR-v5']
+setter_of_fieldnames = ['-','_distance','_direction','_speed','_steering_angle', '_ir_values']
 
 class SonicCar(BaseCar):
 
     def __init__(self):
-        self._start_time = time.time()
         self._us = Ultrasonic() 
-        self._csv_file = open('driving_data.csv', 'w', newline='')
-        self._writer = csv.DictWriter( self._csv_file, fieldnames=fieldnames)
-        self._writer.writeheader()  
         self._distance = 0
         self._distance = self.get_distance()
         super().__init__()
@@ -34,16 +30,4 @@ class SonicCar(BaseCar):
             self._distance = self._us.distance()
         return self._distance
 
-    def __setattr__(self, name, value):
-        # print(f"--> SonicCar setattr: set {name} to {value}")
-        try:
-            object.__setattr__(self, name, value)
-
-            if name in setter_of_fieldnames:
-                t = round(time.time() - self._start_time, 2)
-                data = [{'Time': t, 'Distance': self._distance, 'Direction': self._direction, 'Speed': self._speed, 'Steering': self._steering_angle}]
-                self._writer.writerows(data)
-                print(f"Write CSV: {data}")
-        except Exception as e:
-            print(f"{__name__}: An exception occurred: {e}")
     
