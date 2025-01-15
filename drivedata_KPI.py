508fc1efe28dd5316fe49eb7c7dbe31cad62c6a8
KaiMi92 <kai.michel@me.com>
2025-01-15 13:18:22 +0100

    merge drivedata kpi

--- drivedata_KPI.py
+++ drivedata_KPI.py
@@ -16,7 +16,6 @@ except pd.errors.EmptyDataError:
     skip_var = 1
     
 
-<<<<<<< HEAD
 '''
 .max -> searches for the highest value of the vector
 .min -> searches for the minimum value of the vector
@@ -49,30 +48,3 @@ drive_data['time_diff'] = drive_data["Time"].diff()
 distances = drive_data['time_diff'][1:].values * drive_data['Speed'][:-1].values
 driven_distance = sum(distances)
 print(f'Driven distance:' ,driven_distance)
-=======
-while skip_var == 0:
-
-    data_speed = drive_data["Speed"]
-    data_time = drive_data['Time']
-    sum_time = data_time.sum()
-
-    #max speed
-    max_speed = data_speed.max()
-    print(f'Max speed:',max_speed)
-    #min speed
-    min_speed = data_speed.min()
-    print(f'Min speed:',min_speed)
-    #average speed
-    average_speed = data_speed.mean()
-    print(f'Average speed:', average_speed)
-    #distance traveled
-    #distance_traveled = data_speed.mean()/sum_time
-    distance_traveled = (data_speed * data_time).sum()
-    print(f'Distance traveled:', distance_traveled)
-    #total time
-    print(f'Total time:', sum_time)
-    skip_var = 1
-
-else:
-    print ("No KPI can be written")
->>>>>>> ab9779ea28991d92bb02536513a73e7bcf121539
