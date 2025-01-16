4f6f83ca92e7f301a3aea26e33939a70ebc5edb3
KaiMi92 <kai.michel@me.com>
2025-01-16 16:03:41 +0100

    add turtle diagramm

--- app.py
+++ app.py
@@ -11,13 +11,14 @@ from drivedata_KPI import *
 from basecar import BaseCar
 from driving_mode_4 import *
 from driving_mode_5 import * 
+import math
 
 app = Dash(__name__)
 
 app.layout = html.Div(
     [
         html.H1(["Drive Data Dashboard"], id="my-header"),
-        dcc.Dropdown(["Distance", "Direction", "Speed", "Steering"], value="Distance", id="my-dd"),
+        dcc.Dropdown(["Time", "Direction", "Speed", "Steering", "Distance", "IR-v1", "IR-v2", "IR-v3", "IR-v4", "IR-v5"], value="Steering", id="my-dd"),
         dbc.Col(dcc.Graph(id="my-graph")),
         html.Button('Show KPIs', id='btn-1', n_clicks=0),
         html.Div(id='container-button-timestamp'),
@@ -27,7 +28,11 @@ app.layout = html.Div(
         html.Button('Driving Mode 3', id='b-dm3', n_clicks=0),
         html.Button('Driving Mode 4', id='b-dm4', n_clicks=0),
         html.Button('Driving Mode 5', id='b-dm5', n_clicks=0),
-        html.Div(id='mode-container')
+        html.Div(id='mode-container'),
+        html.H4('Simple stock plot with adjustable axis'),
+        html.P("Change figure width:"),
+        dcc.Slider(id='slider', min=200, max=500, step=25, value=300,marks={x: str(x) for x in [200, 300, 400, 500]}),
+        dcc.Graph(id="graph"),
     ]
 )
 
@@ -87,6 +92,42 @@ def run_drive_modes(btnstop, bdm1, bdm2, bdm3, bdm4, bdm5):
         exec(open(script_path).read())
     return msg_dm
 
+@app.callback(Output("graph", "figure"),Input('slider', 'value'))
+def resize_figure(width):
+    df = read_data()
+    fig = px.line(df, x='x', y='y')  
+    fig.update_layout(
+        margin=dict(l=20, r=20, t=20, b=20),
+        paper_bgcolor="LightSteelBlue",)
+    fig.update_layout(width=int(width))
+ 
+    return fig
+
+def read_data():
+    path = "driving_data/driving_data.csv"
+    drive_data = pd.read_csv(path)
+    data_speed = drive_data["Speed"]
+    data_time = drive_data['Time']
+    data_steering = drive_data['Steering']
+ 
+    x = 0.0
+    y = 0.0
+ 
+    df = pd.DataFrame(columns=["x", "y"])
+    df.loc[0] = [x, y]
+ 
+    for i in range(1, len(drive_data)):
+        # v = s/t --> s = v * t
+        t = data_time[i] - data_time[i-1]
+        v = data_speed[i-1]
+        s = v * t
+        # print(f"Line {i}: v = {v}, s = {s}, t = {t}, steering = {data_steering[i-1]}")
+ 
+        x = x + s * math.cos(math.radians(data_steering[i-1]))
+        y = y + s * math.sin(math.radians(data_steering[i-1]))
+        df.loc[i] = [x, y]
+ 
+    return df
 
 if __name__ == "__main__":
     app.run_server(debug=True, host="localhost", port=1234)
