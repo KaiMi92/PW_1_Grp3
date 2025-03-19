e732046938a625ffc3126e7c0b42d7822a99ba33
KaiMi92 <kai.michel@me.com>
2025-03-19 15:00:45 +0100

    add steering modus 3 kai

--- calc_steering_angle_3.py
+++ calc_steering_angle_3.py
@@ -1,2 +1,31 @@
+#from line_chart import *
+# RS-Werte (30 - 110), (106 - 150), (26 - 86)
 def calculate_steering_angle(image, lines):
-    return 90
+    sum = 0
+    runs = 0
+    average = 0
+    if lines is not None:
+        for line in lines:
+            x1, y1, x2, y2 = line[0]
+            
+            # Berechnung der Steigung
+            if (x2 - x1) != 0:
+                slope = (y2 - y1) / (x2 - x1)
+            else:
+                continue
+            #print(slope)
+            runs += 1
+            sum += slope
+            average = sum/runs
+        print(average)
+
+    x = 90
+    if average > 0 and average <3:
+        x = x - (average * 50)
+    elif average < 0 and average > -3:
+        x = x + (abs(average) * 50)
+    print(x)
+    steering_angle = x
+    return steering_angle
+
+#calculate_steering_angle()
