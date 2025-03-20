86f85c6cb63a3676afef0c624f63caaa64801276
Sebastian <sebastian.lorenz@volkswagen.de>
2025-03-20 10:19:06 +0100

    Nicht geblurtes Bild anzeigen

--- processor.py
+++ processor.py
@@ -29,9 +29,9 @@ class Processor():
         # Parkettfugen wurden als Linien erkannt
         # daher erst MedianBlur, um Fugen zu verwischen
         # img_crop = cv2.medianBlur(img_crop,7)
-        img_crop = cv2.blur(img_crop,(9, 9))
+        img_blur = cv2.blur(img_crop,(9, 9))
 
-        img_hsv = cv2.cvtColor(img_crop, cv2.COLOR_BGR2HSV)
+        img_hsv = cv2.cvtColor(img_blur, cv2.COLOR_BGR2HSV)
         lower_blue = np.array([self.lower_hue, self.lower_saturation, self.lower_value])
         upper_blue = np.array([self.upper_hue, self.upper_saturation, self.upper_value])
         mask = cv2.inRange(img_hsv, lower_blue, upper_blue)
