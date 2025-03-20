6d92c8613dbc859f6b6c282dc2c64296465552ee
KaiMi92 <kai.michel@me.com>
2025-03-20 13:44:51 +0100

    fix lenkwinkel multiplier

--- calc_steering_angle_3.py
+++ calc_steering_angle_3.py
@@ -21,12 +21,12 @@ def calculate_steering_angle(image, lines):
 
     x = 90
     if average > 0 and average <3:
-        x = x - (average * 50)
+        x = x - (average * 70)
         if x < 45:
             x = 45
             print("Maximum Lenkwinkel von 45 Grad erreicht")
     elif average < 0 and average > -3:
-        x = x + (abs(average) * 50)
+        x = x + (abs(average) * 70)
         if x > 135:
             x = 135
             print("Maximum Lenkwinkel von 135 Grad erreicht")
