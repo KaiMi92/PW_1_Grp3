14bdc8ad2fa4d401131a24d94ced05b39d1acc5b
Kai <kai.michel@me.com>
2025-03-27 11:19:10 +0100

    add Kommentare zu Lenkwinkel Berechnung Kai steering angle 3

--- calc_steering_angle_3.py
+++ calc_steering_angle_3.py
@@ -1,7 +1,13 @@
 import time
 
-#from line_chart import *
-# RS-Werte (30 - 110), (106 - 150), (26 - 86)
+"""
+    !!! Generelle Information !!!
+    Diese Funktion berechnet den Lenkwinkel des Autos.
+    Dazu wird die Steigung der gesamten Linien berechnet, die durch die Hough-Transformation gefunden wurden.
+    Der Durchschnitt der Steigungen wird dann in einen Lenkwinkel umgerechnet.
+    Der Lenkwinkel wird dann zurückgegeben.
+"""
+
 def calculate_steering_angle(image, lines):
     sum = 0
     runs = 0
@@ -10,31 +16,40 @@ def calculate_steering_angle(image, lines):
         for line in lines:
             x1, y1, x2, y2 = line[0]
             
-            # Berechnung der Steigung
+            """ 
+            Berechnung der Steigung 
+            """
             if (x2 - x1) != 0:
                 slope = (y2 - y1) / (x2 - x1)
             else:
                 continue
-            #print(slope)
+
+            """
+            Berechnung des Durchschnitts der Steigungen
+            """
+
             runs += 1
             sum += slope
             average = sum/runs
-        #print(average)
+
+    """""
+    Umrechnung der Steigung in einen Lenkwinkel
+    Der Lenkwinkel wird auf einen Wert zwischen 45 und 135 begrenzt"
+    """
 
     x = 90
     if average > 0 and average <3:
         x = x - (average * 70)
         if x < 45:
             x = 45
-            #print("Maximum Lenkwinkel von 45 Grad erreicht")
+
     elif average < 0 and average > -3:
         x = x + (abs(average) * 70)
         if x > 135:
             x = 135
-            #print("Maximum Lenkwinkel von 135 Grad erreicht")
-    #print(x)
+
     steering_angle = x
-    #time.sleep(0.1)
+
     return steering_angle
 
 #calculate_steering_angle()
